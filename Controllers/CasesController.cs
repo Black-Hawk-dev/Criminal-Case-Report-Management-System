d27cacf513337cf5fd1121048057da84feeaefa7
Ahmed shahwan <blackhawk300018@gmail.com>
2025-07-27 13:51:18 +0300

    Update CasesController.cs

--- Controllers/CasesController.cs
+++ Controllers/CasesController.cs
@@ -1,11 +1,3 @@
-using Microsoft.AspNetCore.Mvc;
-using Microsoft.AspNetCore.Mvc.Rendering;
-using Microsoft.EntityFrameworkCore;
-using Microsoft.AspNetCore.Authorization;
-using CriminalCaseManagement.Data;
-using CriminalCaseManagement.Models.Entities;
-using CriminalCaseManagement.Models.ViewModels;
-
 namespace CriminalCaseManagement.Controllers
 {
     [Authorize]
