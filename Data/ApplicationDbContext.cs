34753c16124d549dd953303d7d5386bb032669b1
Ahmed shahwan <blackhawk300018@gmail.com>
2025-07-28 22:36:41 +0300

    Update ApplicationDbContext.cs

--- Data/ApplicationDbContext.cs
+++ Data/ApplicationDbContext.cs
@@ -1,6 +1,3 @@
-using Microsoft.AspNetCore.Identity.EntityFrameworkCore;
-using Microsoft.EntityFrameworkCore;
-using CriminalCaseManagement.Models.Entities;
 
 namespace CriminalCaseManagement.Data
 {
